908977c9d1db7bfc65b6b79acd517c28b3cde9ad
rohit kumar <raju0030@algonquinlive.com>
2024-09-26 13:00:55 -0700

    lab

--- app/src/main/java/com/example/androlabs/MainActivity.java
+++ app/src/main/java/com/example/androlabs/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.androlabs;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Button pressMeButton = findViewById(R.id.button);
+
+
+        pressMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Button Pressed!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
